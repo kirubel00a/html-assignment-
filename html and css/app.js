efd26baf57fc185845d5f208f27c68627c6888b4
kirubel <kirubel@LAPTOP-P1MTFSQK>
2024-03-08 23:31:50 +0100

    assigment for school

--- html and css/app.js
+++ html and css/app.js
@@ -2,7 +2,20 @@ const menu = document.querySelector('#mobile_menu');
 const menulinks = document.querySelector('.navbar_menu');
 
 menu.addEventListener('click', function(){
-    menu.classlist.toggle('is-active');
-    menulinks.classlist.toggle('active');
+    menu.classList.toggle('is-active');
+    menulinks.classList.toggle('active');
     
 });
+
+function swapImages() {
+    var image1 = document.getElementById('image1');
+    var image2 = document.getElementById('image2');
+
+    // Get the current source of each image
+    var src1 = image1.src;
+    var src2 = image2.src;
+
+    // Swap the source of the images
+    image1.src = src2;
+    image2.src = src1;
+}
