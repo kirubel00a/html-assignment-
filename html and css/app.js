e3e2d493665ca94d0bec5f4d2851c7e74849ba75
kirubel <kirubel@LAPTOP-P1MTFSQK>
2024-03-18 00:49:21 +0100

    assigment for school

--- html and css/app.js
+++ html and css/app.js
@@ -1,23 +1,34 @@
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slidesfade");
-  if (n > slides.length) { slideIndex = 1; }
-  if (n < 1) { slideIndex = slides.length; }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var info = document.getElementsByClassName("information");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[i].classList.remove("active");  
+    }
+    for (i = 0; i < info.length; i++) {
+        info[i].style.display = "none";  
+    }
+    slides[slideIndex - 1].classList.add("active");
+    info[slideIndex-1].style.display = "block";
+    updateSlidePosition();
+}
+function updateSlidePosition() {
+  var container = document.querySelector('.slideshow_container');
+  var activeSlide = document.querySelector('.slide.active');
+  var scrollLeft = activeSlide.offsetLeft - (container.offsetWidth - activeSlide.offsetWidth) / 2;
+  container.scrollLeft = scrollLeft;
 }
